f00ced6ac60d46d9f78d1968bbdc62bd92be4d3d
Olga <oaglazunova@gmail.com>
2016-01-14 18:59:37 +0300

    Updated

--- js/script.js
+++ js/script.js
@@ -1,78 +1,77 @@
-//Masonry options
+'use strict';
+
 $(".js-grid").masonry({
   itemSelector: ".js-grid-item",
-  columnWidth: 300,
+  columnWidth: 220,
   "gutter": 20,
   isFitWidth: true,
   stamp: '.js-stamp'
 });
-// masonry options end
 
+var lh = [];
+var wscroll = 0;
+var wh = $(window).height();
 
+function updateOffsets() {
+  $(".js-lazy").each(function() {
+    var x = $(this).offset().top;
+    lh.push(x);
+  });
+}
 
+function lazy() {
+  wscroll = $(window).scrollTop();
 
+  for (var i = 0; i < lh.length; i++) {
+    if (lh[i] <= (wscroll + (wh - 200))) {
+      var blockClassName = $(".js-lazy").eq(i).attr("data-attr");
+      $(".js-lazy").eq(i).addClass(blockClassName);
+    }
+  }
+}
 
-//Refreshes layout after screen size change 
-/*
-$(window).bind('resize', function(e)
-{
-  if (window.RT) clearTimeout(window.RT);
-  window.RT = setTimeout(function()
-  {
-    this.location.reload(false); // false to get page from cache 
-  }, 100);
+$(window).on('scroll', function() {
+  lazy();
 });
-*/
-//refresh end
 
-
-
-//Helpers
 function getChildByClassName(parent, className) {
   for (var i = 0; i < parent.children.length; i++) {
-    if (parent.children[i].classList.contains(className))
+    if (parent.children[i].classList.contains(className)) {
       return parent.children[i];
+    }
   }
 }
 
 function getParentByClassName(child, className) {
-    while (child.parentNode) {
-      child = child.parentNode;
-      if (child.classList.contains(className))
-        return child;
+  while (child.parentNode) {
+    child = child.parentNode;
+    if (child.classList.contains(className)) {
+      return child;
     }
-    return null;
   }
-  //Helpers end
+  return null;
+}
 
-
-
-//slider id gen 
 function generateSliderId(id) {
-    return "slider-" + id;
-  }
-  //slider id gen end
+  return "slider-" + id;
+}
 
-
-//slider, popup, video - init
-$(document).ready(function () {
-  $(".blocks__item").each(function () {
+$(document).ready(function() {
+  $(".blocks__item").each(function() {
     var obj = $(this);
     var id = 1;
-    $(obj).find("li").each(function () {
+    $(obj).find("li").each(function() {
       $(this).addClass("js-slide-" + id);
       id++;
     });
 
-    //insert controls into slider
-    $(obj).append("<div class=\"blocks__slider-controls\">\
-              <span class=\"slider-arrow  slider-arrow--prev\"><svg width=\"23\" height=\"22\" preserveaspectratio=\"xMidYMid\"><use xmlns:xlink=\"http://www.w3.org/1999/xlink\" xlink:href=\"#icon-prev\"></use></svg></span>\
-              <span class=\"slider-arrow  slider-arrow--next\"><svg width=\"23\" height=\"22\" preserveaspectratio=\"xMidYMid\"><use xmlns:xlink=\"http://www.w3.org/1999/xlink\" xlink:href=\"#icon-next\"></use></svg></span>\
+    $(obj).append("<div class=\'blocks__slider-controls\'>\
+              <span class=\'slider-arrow  slider-arrow--prev\'><svg width=\'23\' height=\'22\' preserveaspectratio=\'xMidYMid\'><use xmlns:xlink=\'http://www.w3.org/1999/xlink\' xlink:href=\'#icon-prev\'></use></svg></span>\
+              <span class=\'slider-arrow  slider-arrow--next\'><svg width=\'23\' height=\'22\' preserveaspectratio=\'xMidYMid\'><use xmlns:xlink=\'http://www.w3.org/1999/xlink\' xlink:href=\'#icon-next\'></use></svg></span>\
             </div>");
   });
-  // end insert controls into slider
 
-  var sliders = document.getElementsByClassName("blocks__item");
+  var sliders = $(".blocks__item");
   for (var i = 0; i < sliders.length; i++) {
     var sliderId = generateSliderId(i);
     sliders[i].id = sliderId;
@@ -103,111 +102,60 @@ $(document).ready(function () {
   function moveLeft(id) {
     $(id + ' ul').animate({
       left: +slideWidth
-    }, 200, function () {
+    }, 200, function() {
       $(id + ' ul li:last-child').prependTo(id + ' ul');
       $(id + ' ul').css('left', '');
     });
-    //update screen to lazy load hidden sliders
+
     lazy();
-  };
+  }
 
   function moveRight(id) {
     $(id + ' ul').animate({
       left: -slideWidth
-    }, 200, function () {
+    }, 200, function() {
       $(id + ' ul li:first-child').appendTo(id + ' ul');
       $(id + ' ul').css('left', '');
     });
-    //update screen to lazy load hidden sliders
-    lazy();
-  };
 
-  $('.slider-arrow--prev').click(function () {
+    lazy();
+  }
+
+  $('.slider-arrow--prev').click(function() {
     var id = getParentId(this);
     moveLeft("#" + id);
   });
 
-  $('.slider-arrow--next').click(function () {
+  $('.slider-arrow--next').click(function() {
     var id = getParentId(this);
     moveRight("#" + id);
   });
-  //end slider part
 
-
-  
-  //popup (cont)
-  $(".js-slide-3").each(function () {
+  $(".js-slide-3").each(function() {
     var zoomSvg = getChildByClassName(this, "blocks__btn");
-    var popupImg = document.getElementById("popup-" + zoomSvg.id);
-    $(zoomSvg).click(function () {
-      popupImg.classList.add("show");
+    $(zoomSvg).click(function() {
+      $("#popup-" + zoomSvg.id).addClass("show");
     });
   });
-  //popup not end  
 
-
-
-  // vidup (cont)
-  $(".js-slide-4").each(function () {
+  $(".js-slide-4").each(function() {
     var playSvg = getChildByClassName(this, "blocks__btn");
-    var vidupVid = document.getElementById("popup-" + playSvg.id);
-    $(playSvg).click(function () {
-      vidupVid.classList.add("show");
+    $(playSvg).click(function() {
+      $("#popup-" + playSvg.id).addClass("show");
     });
   });
-  //vidup not end
 
-
-  // Lazy load init
-  update_offsets();
+  updateOffsets();
   lazy();
-  // lazy load init end  
 
 });
-// slider, popup, video - end init
 
-
-
-//  Lazy Load for bkg img
-var ll = $(".js-lazy");
-var lh = []
-var wscroll = 0;
-var wh = $(window).height();
-
-function update_offsets() {
-  $(".js-lazy").each(function () {
-    var x = $(this).offset().top;
-    lh.push(x);
-  });
-};
-
-function lazy() {
-  wscroll = $(window).scrollTop();
-
-  for (i = 0; i < lh.length; i++) {
-    if (lh[i] <= (wscroll + (wh - 200))) {
-      var blockClassName = $(".js-lazy").eq(i).attr("data-attr");
-      $(".js-lazy").eq(i).addClass(blockClassName);
-    }
-  };
-};
-
-$(window).on('scroll', function () {
-  lazy();
-});
-//lazy load end
-
-
-
-
-//  Video auto-start
 var tag = document.createElement('script');
 tag.src = "http://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var players = {};
 
-// this function gets called when API is ready to use
 function onYouTubePlayerAPIReady() {
   var videos = document.getElementsByClassName("js-video");
   for (var i = 0; i < videos.length; i++) {
@@ -215,58 +163,47 @@ function onYouTubePlayerAPIReady() {
       events: {
         "onReady": onPlayerReady(videos[i].id)
       }
-    }
+    };
   }
 }
 
 function onPlayerReady(frameId) {
-    // bind events
-    var playBtn = document.getElementById("btn-" + frameId);
-    playBtn.addEventListener("click", function () {
-      var player = players[frameId];
-      player.playVideo();
-    });
-  }
-  // end video auto-start
+  var playBtn = document.getElementById("btn-" + frameId);
+  playBtn.addEventListener("click", function() {
+    var player = players[frameId];
+    player.playVideo();
+  });
+}
 
-
-
-
-//popup (cont)
-$(".popups__close-pict").click(function () {
+$(".popups__close-pict").click(function() {
   var popupImg = getParentByClassName(this, "popups__window");
   popupImg.classList.remove("show");
 });
-//end popup
 
-
-
-// vidup (cont)
-$(".popups__close-vid").click(function () {
+$(".popups__close-vid").click(function() {
   var vidupImg = getParentByClassName(this, "popups__window");
   var popupsVid = getParentByClassName(this, "popups__vid");
   var frameId = getChildByClassName(popupsVid, "js-video");
+  //  var players = {};
   vidupImg.classList.remove("show");
   var player = players[$(frameId).attr("id")];
   player.stopVideo();
 });
-//end vidup
 
 
-
-// Filter
 var filters = document.getElementsByClassName("filter__item");
 
 function toggleModifier(selector, modifier, self) {
   var x = document.getElementsByClassName(selector);
-  for (var i = 0; i < x.length; i++)
+  for (var i = 0; i < x.length; i++) {
     x[i].classList.remove(modifier);
+  }
   self.classList.add(modifier);
 }
 
 function shrinkBlock(filterButton, blockToFilter, self) {
   if (self.classList.contains(filterButton)) {
-    var x = document.getElementsByClassName("blocks__item");
+    var x = $(".blocks__item");
     for (var i = 0; i < x.length; i++) {
       x[i].classList.remove("shrink");
       if (x[i].classList.contains(blockToFilter)) {
@@ -278,7 +215,7 @@ function shrinkBlock(filterButton, blockToFilter, self) {
 }
 
 function growBlocks(self) {
-  var x = document.getElementsByClassName("blocks__item");
+  var x = $(".blocks__item");
   for (var i = 0; i < x.length; i++) {
     if (x[i].classList.contains("shrink")) {
       x[i].classList.add("grow");
@@ -286,7 +223,7 @@ function growBlocks(self) {
   }
 }
 
-var doFilter = function () {
+var doFilter = function() {
   toggleModifier("filter__item", "filter__item--active", this);
   growBlocks(this);
   shrinkBlock("js-show-all", "js-filter-all", this);
@@ -294,8 +231,12 @@ var doFilter = function () {
   shrinkBlock("js-filter2", "js-filter-not2", this);
   shrinkBlock("js-filter3", "js-filter-not3", this);
   shrinkBlock("js-filter4", "js-filter-not4", this);
+  shrinkBlock("js-filter5", "js-filter-not5", this);
+  shrinkBlock("js-filter6", "js-filter-not6", this);
+  shrinkBlock("js-filter7", "js-filter-not7", this);
+  shrinkBlock("js-filter8", "js-filter-not8", this);
 }
 
-for (var i = 0; i < filters.length; i++)
+for (var i = 0; i < filters.length; i++) {
   filters[i].addEventListener("click", doFilter, false);
-// end filter
+}
